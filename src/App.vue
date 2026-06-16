20ade920b00c09756c357b9f13fcb79d58e9a23c
Administrator <admin@example.com>
2026-06-16 16:16:11 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <n-config-provider>
+    <n-message-provider>
+      <router-view />
+    </n-message-provider>
+  </n-config-provider>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home {
+  padding: 24px;
+
+  h1 {
+    color: #333;
+  }
+}
+</style>
